7187f7272ee9b05df796e96a5c15d3efe4e50447
Pushkar Kumar <108152620+Pushkar325@users.noreply.github.com>
2023-02-28 16:27:28 +0530

    Added Todo list

---
+++ javascript.js
@@ -0,0 +1,57 @@
+var input_field = document.querySelector("#list"); 
+var button = document.querySelector(".but");
+var block = document.querySelector(".todos");
+
+//When button is pressed
+button.addEventListener("click",function(){
+    var string = input_field.value;
+
+    if(string === "" || string == " "){
+        document.querySelector(".error").innerHTML = "Kuch daal toh bhai";
+    }
+
+    else{
+        
+        //Error ko reset kardiya hai yaha par
+        document.querySelector(".error").innerHTML="";
+
+        //Create a empty paragraph
+        var para = document.createElement("li");
+        para.innerText = string;
+        // block.appendChild(para);
+        var spa = document.createElement("span");
+        spa.innerHTML = "x";
+        spa.className = "close";
+        spa.addEventListener("click",closee);
+        para.appendChild(spa);
+
+        //Single click karne par
+        para.addEventListener("click",function(){
+            para.style.textDecoration = "line-through";
+            para.style.backgroundColor = "grey";
+
+        });
+
+        //Double click karne par
+        para.addEventListener("dblclick",function(){
+            para.style.textDecoration = "none";
+            para.style.backgroundColor = "white";
+
+        });
+
+        //Single click kar function
+        function closee(){
+            block.removeChild(para);
+        }
+
+        block.appendChild(para);
+        
+    }
+    //Reseting the value of the Input Field
+    document.querySelector("#list").value = null;
+}
+);
+
+
+
+
